e13294a87c2562e86fb9837d4c8a253dd7c7a8a1
Alexis <alexis@ladydinde.com>
2022-05-06 17:00:03 +0200

    Add a clear option before setting a value for the led to blink, and add a delay parameter for the display to wait a bit before setting the value

--- libraries/FourLeds/FourLeds.cpp
+++ libraries/FourLeds/FourLeds.cpp
@@ -14,11 +14,24 @@ FourLeds::FourLeds(int pin0, int pin1, int pin2, int pin3)
      
      for (int i = 0; i < 4; i++)
     {
-        pinMode(_pins[i], OUTPUT);
+      pinMode(_pins[i], OUTPUT);
     }
 }
 
-void FourLeds::setValue(int value){
+void FourLeds::setValue(int value, bool clear, int delay){
+  _value = value;
+  _time = millis();
+  _delay = delay;
+  if(clear){    
+    for( int i = 0;i<4;i++ ){
+      analogWrite(_pins[i],0);
+    }
+    return;
+  }
+  if(delay){
+    return;
+  }
+  _time = 0;
   int base = value >> 2; //divide by 4
   int left = value % 4; // what's left when you divide by 4
   for( int i = 0;i<4;i++ ){
@@ -28,5 +41,9 @@ void FourLeds::setValue(int value){
     } 
     analogWrite(_pins[i], max(0, brightness-1) );
   }
-
+}
+void FourLeds::update(long elapsed){
+  if(_time!=0 && ((elapsed-_delay) - _time ) > 100){
+    setValue(_value,false);
+  }
 }
--- libraries/FourLeds/FourLeds.h
+++ libraries/FourLeds/FourLeds.h
@@ -1,5 +1,7 @@
 /*
-  FourLeds.h - Library for Four Leds.
+*  FourLeds.h - Library for Four Leds.
+*  04_leds_display_library 
+*   initial class 
 */
 #ifndef FourLeds_h
 #define FourLeds_h
@@ -8,9 +10,12 @@ class FourLeds
 {
 public:
   FourLeds(int pin0, int pin, int pin2, int pin3);
-  void setValue(int value);
+  void update(long time);
+  void setValue(int value, bool clear=false, int delay=0);
 private:
   int _pins[4];
+  int _value;
+  int _delay;
+  long _time;
 };
-
 #endif
